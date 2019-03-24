a11772d32c0067a910c4331e700d715912ccc721
Lee-Dong-Wook <dongwookRaynor@gmail.com>
2019-03-24 12:42:40 +0900

    OpenCV Chapter3_task 14,19
    14. Rect객체 생성 후 좌표이동, size 변경 후 출력
    
    19. Mat 행렬 객체 생성 후 Range()를 사용하여 행렬 덮어 쓰기

--- openCV/Chapter3_task14/Chapter3_task14.cpp
+++ openCV/Chapter3_task14/Chapter3_task14.cpp
@@ -1,20 +1,31 @@
 #include <opencv2/highgui.hpp>
-#include <time.h>
 #include <iostream>
 
 using namespace cv;
 using namespace std;
 
 int main() {
- // Rect 클래스 사용하면 됨 ->시작 좌표 30,40  크기가 100, 200 객체 Rect 
-
+  
 	//좌표를 10,10 평행이동 , 20x30만큼 증가  // 포인트 연산, 사이즈와 연산 
+	//size.width, size.hieght, size.area(), size 객체 명은 가로x세로 출력 
 
-	Point2i StartPoint(30,40);
-	Size2f RectSize(100, 200);
+	//사각형 생성 Rect(시작좌표x,시작좌표y,너비w,높이h)
+	Rect_<float>  rect(30.0f, 40.0f, 100.0f, 200.0f);
 
+	cout << " 초기사각형 정보 "<< rect << endl; 
+	cout << " 초기사각형의 넓이 "<<rect.area() << endl<<endl; 
+
+	//좌표와 크기 객체 생성 
+	Size2f sz(20,30);
+	Point2f pt(10, 10);
+
+	rect = rect + sz;
+	cout << " 크기 조절 후 사각형 정보 " << rect << endl;
+	cout << " 크기 조절 후 사각형 넓이 " << rect.area() << endl<<endl;
+	
+	rect = rect + pt; 
+	cout << " 좌표 이동 후 사각형 정보 " << rect << endl;
 	
 
-	
 	return 0;
 }
---
+++ openCV/Chapter3_task19/Chapter3_task19.cpp
@@ -0,0 +1,33 @@
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+
+//void CropMax(Mat matx, int start_point1, int start_point2, int end_point1, int endPoint2, int setNumber)
+void CropMatx(Mat m, int s_pt1, int s_pt2 ,int e_pt1, int e_pt2 , int setNumber) {
+
+	Mat	  matx; 
+	Range start(s_pt1, s_pt2);
+	Range end(e_pt1, e_pt2);
+
+	matx = m(start, end);
+	matx.setTo(setNumber);
+}
+
+int main() {
+
+	//int형 행렬, 10x15크기 Mat클래스 사용 
+	//범위 지정하는 함수 생성 Range()와 setTo()사용 
+	//부모 행렬 입력, 범위지정할 좌표2개 입력,변경할 값입력
+
+	Mat mat(10, 15, CV_32F, Scalar(100));
+
+	CropMatx(mat, 1, 5, 3, 8, 200);
+	CropMatx(mat, 5, 9, 8, 14, 300);
+	CropMatx(mat, 3, 7, 5, 10, 555);
+	
+	cout << mat << endl; 
+	return 0;
+}
